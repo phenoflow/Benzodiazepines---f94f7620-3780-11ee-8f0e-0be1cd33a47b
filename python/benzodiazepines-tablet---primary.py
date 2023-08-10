0fb4b063803a8d65404ba24116df875ccb10d4d5
Martin Chapman <contact@martinchapman.co.uk>
2023-08-10 14:26:03 +0100

    Update made by Phenoflow

---
+++ python/benzodiazepines-tablet---primary.py
@@ -0,0 +1,25 @@
+# Matthew J Carr, Darren M Ashcroft, Evangelos Kontopantelis, David While, Yvonne Awenat, Jayne Cooper, Carolyn Chew-Graham, Nav Kapur, Roger T Webb, 2023.
+
+import sys, csv, re
+
+codes = [{"code":"41200020","system":"multilex"},{"code":"48237020","system":"multilex"},{"code":"48605020","system":"multilex"},{"code":"50709020","system":"multilex"},{"code":"50737020","system":"multilex"},{"code":"59073020","system":"multilex"},{"code":"59078020","system":"multilex"},{"code":"64054020","system":"multilex"},{"code":"65074020","system":"multilex"},{"code":"65258020","system":"multilex"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('benzodiazepines-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["benzodiazepines-tablet---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["benzodiazepines-tablet---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["benzodiazepines-tablet---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
