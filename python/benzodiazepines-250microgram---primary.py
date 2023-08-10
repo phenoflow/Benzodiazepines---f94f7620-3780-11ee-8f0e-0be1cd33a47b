db583a1b1a93b231cce60857473c6ffe1f91f76c
Martin Chapman <contact@martinchapman.co.uk>
2023-08-10 14:27:55 +0100

    Update made by Phenoflow

---
+++ python/benzodiazepines-250microgram---primary.py
@@ -0,0 +1,25 @@
+# Matthew J Carr, Darren M Ashcroft, Evangelos Kontopantelis, David While, Yvonne Awenat, Jayne Cooper, Carolyn Chew-Graham, Nav Kapur, Roger T Webb, 2023.
+
+import sys, csv, re
+
+codes = [{"code":"445021","system":"multilex"},{"code":"51447020","system":"multilex"},{"code":"60532020","system":"multilex"},{"code":"60685020","system":"multilex"},{"code":"64060020","system":"multilex"},{"code":"89956020","system":"multilex"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('benzodiazepines-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["benzodiazepines-250microgram---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["benzodiazepines-250microgram---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["benzodiazepines-250microgram---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
---
+++ python/benzodiazepines-librium---primary.py
@@ -0,0 +1,25 @@
+# Matthew J Carr, Darren M Ashcroft, Evangelos Kontopantelis, David While, Yvonne Awenat, Jayne Cooper, Carolyn Chew-Graham, Nav Kapur, Roger T Webb, 2023.
+
+import sys, csv, re
+
+codes = [{"code":"50187020","system":"multilex"},{"code":"50188020","system":"multilex"},{"code":"50189020","system":"multilex"},{"code":"50193020","system":"multilex"},{"code":"50194020","system":"multilex"},{"code":"88274020","system":"multilex"},{"code":"88276020","system":"multilex"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('benzodiazepines-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["benzodiazepines-librium---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["benzodiazepines-librium---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["benzodiazepines-librium---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
